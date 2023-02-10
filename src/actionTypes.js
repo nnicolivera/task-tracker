71aa5e1c52295ef42e09f26498b1efe36dd4b63c
Nicolás Olivera <nnicolivera@hotmail.com>
2023-02-10 13:48:45 -0300

    add one and get all tasks with redux global state

---
+++ src/actionTypes.js
@@ -0,0 +1,4 @@
+export const ADD_ITEM = "ADD_ITEM";
+export const RESET_LIST = "RESET_LIST";
+export const SET_LIST = "SET_LIST"
+export const SET_COLOR = "SET_COLOR"
--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,28 @@
-export const changeColor = color => ({
-    type: "color",
-    payload: {
-        color
-    }
-})
+import * as actions from './actionTypes'
 
-export const getTasks = tasks => ({
-    type: "getTasks",
-    payload: {
-        tasks
-    }
-})
+export const changeColor = color => {
+    return { type: actions.SET_COLOR, payload: color };
+    // type: "color",
+    // payload: {
+    //     color
+    // }
+}
+
+export const setList = items => {
+    return { type: actions.SET_LIST, payload: items };
+}
+
+export const addItem = item => {
+    return { type: actions.ADD_ITEM, payload: item };
+}
+
+export const resetList = () => {
+    return { type: actions.RESET_LIST };
+}
+
+// export const getTasks = tasks => ({
+//     type: "getTasks",
+//     payload: {
+//         tasks
+//     }
+// })
